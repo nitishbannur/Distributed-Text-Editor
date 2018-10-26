a0876ab80f9fefd6f883f540de452a76465b0ebe
Anurag Katakkar <anuraglkatakkar@gmail.com>
2018-10-26 22:28:21 +0530

    Add files via upload

--- DS/cli_gui.py
+++ DS/cli_gui.py
@@ -5,7 +5,7 @@ import sys
 import datetime 
 import time
 import random
-
+import os.path
 """GUI Part"""
 
 
@@ -19,8 +19,6 @@ root=Tk()   #First Screen+
 CS=0                            #initally, root holds the token to the critical section
 w=(root.winfo_screenwidth())/2
 h=(root.winfo_screenheight())/2
-
-#print("width is"+str(w)+"height is: "+str(h))
 
 root.geometry("%dx%d+0+0"%(w,h))
 
@@ -38,9 +36,10 @@ Editor = Text(root, width=50, height=15)
 message_from_gui = ""
 
 def writer():
-    Editor.place(x=30, y=100)
-    submit.place(x=30, y=360)
-    update.place(x=120, y=360)
+    Editor.place(x=(w/4), y=100)
+    submit.place(x=(w/2)-(w/4), y=360)
+    update.place(x=(w/2)+(w/4)-75, y=360)
+    save.place(x=(w/2)-(w/4), y=390)
 
 
 def send_message():
@@ -49,7 +48,7 @@ def send_message():
     print("\nMessage from GUI ", message)
     message = message+"\t"+str(datetime.datetime.now()) 
     server.send(message.encode()) 
-    sys.stdout.write("<You>") 
+    sys.stdout.write("\n<You>") 
     sys.stdout.write(message) 
     sys.stdout.flush()
 
@@ -58,9 +57,22 @@ def recv_message():
 	print(message)
 	Editor.delete("1.0", "end-1c")
 	Editor.insert("1.0", message)
+	
+def save_message():
+	message_from_gui = Editor.get("1.0", "end-1c")
+	with open("TextEditor.txt", "w", encoding='UTF-8') as f:
+		f.write(message_from_gui)
+		
+def load_msg():
+	with open("TextEditor.txt", "r", encoding='UTF-8') as f:
+		message = f.read()
+	print(message)
+	Editor.delete("1.0", "end-1c")
+	Editor.insert("1.0", message)
 
 submit = Button(root, text="Submit", bg="white", font="bold",command=lambda: send_message())
 update = Button(root, text="Update", bg="white", font="bold",command=lambda: recv_message())
+save = Button(root, text="Save", bg="white", font="bold",command=lambda: save_message())
 writer()
 
 """GUI Part End"""
@@ -72,11 +84,13 @@ if len(sys.argv) != 3:
 IP_address = str(sys.argv[1]) 
 Port = int(sys.argv[2]) 
 server.connect((IP_address, Port)) 
+
+recv_message()
+if (os.path.isfile("TextEditor.txt")):
+	load_msg()
+else:
+	recv_message()
   
-while True: 
-    
-    recv_message()
-      
-    root.mainloop()
+root.mainloop()
 
 server.close()
